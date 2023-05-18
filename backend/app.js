81d4290967244dd380c03068ac7c072fc184f7ee
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-05-18 22:35:47 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const postsRoutes = require("./routes/posts");
 const app = express();
 app.use(cors());
 
-mongoose.connect('mongodb+srv://Hairy--Breeches:KTscpPR9KFTjPgQE@cluster0.cuylh78.mongodb.net/node-angular?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Hairy--Breeches:zt1tKUt2YEDEsuvA@cluster0.cuylh78.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
   console.log('Connected to database!')
 })
