3d3194538720fc5967d0d63be79351927949be09
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-05-18 23:39:04 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,19 @@ const Post = require('./models/posts');
 const postsRoutes = require("./routes/posts");
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
 
 mongoose.connect('mongodb+srv://Hairy--Breeches:zt1tKUt2YEDEsuvA@cluster0.cuylh78.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
