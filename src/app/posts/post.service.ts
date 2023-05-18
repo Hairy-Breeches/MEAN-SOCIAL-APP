714c266086fc6d5cc81e08a8148accb3ea04261d
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-05-19 00:39:57 +0500

    Update post.service.ts

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -22,7 +22,7 @@ export class PostService {
   }
 
   getPost() {
-    this.http.get<{message: string, posts: {_id: string, title: string, content: string, __v: number}[]}>('http://13.53.72.154/api/posts')
+    this.http.get<{message: string, posts: {_id: string, title: string, content: string, __v: number}[]}>('http://16.171.61.235/api/posts')
     .pipe(map(responseData => {
       return responseData.posts.map((post: {_id: string, title: string, content: string, __v: number}) => {
         return {
