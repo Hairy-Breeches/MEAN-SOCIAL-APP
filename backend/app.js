4dea69526faa03e3a7134986f1208fe1ef241131
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-05-18 20:24:20 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,15 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/", express.static(path.join(__dirname,"angular")));
 
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-
-//   next();
-// });
-
 app.use("/api/posts", (req, res, next) => {
   const post = new Post({
     title: req.body.title,
@@ -50,33 +41,13 @@ app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "angular","index.html"));
 })
 
-// app.post('/api/posts', (req, res, next) => {
-//   const post = new Post({
-//     title: req.body.title,
-//     content: req.body.content
-//   });
+app.get('/api/posts', (req, res, next) => {
+  Post.find().then(documents => {
 
-//   post.save();
-
-//   console.log('post: ', post);
-
-
-//   res.status(200).json({
-//     message: 'successful posting!'
-//   });
-// })
-
-// app.get('/api/posts', (req, res, next) => {
-//   Post.find().then(documents => {
-//     res.status(200).json({
-//       message: 'Response from the server!',
-//       posts: documents
-//     });
-//   })
-
-
-
-
-// })
+  res.status(200).json({
+    message: 'Response from the server!',
+    posts: documents
+  });
+})
 
 module.exports = app;
