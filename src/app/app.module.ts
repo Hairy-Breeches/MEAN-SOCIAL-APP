33419b1b7cded388a7ef7135cec10f7c8278a82d
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-05-19 01:58:23 +0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'
-import { RouterModule } from '@angular/router';
+<!-- import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router'; -->
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 
-import { Route } from '@angular/router';
-
-const appRoutes: Route[] = [
+<!-- import { Route } from '@angular/router';
+ -->
+<!-- const appRoutes: Route[] = [
   // {path: '/', component: PostCreateComponent},
   {path: 'api/posts', component: PostListComponent}
-]
+] -->
 
 @NgModule({
   declarations: [
@@ -28,8 +28,6 @@ const appRoutes: Route[] = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
