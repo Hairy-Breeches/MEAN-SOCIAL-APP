9bed0734059ad4a18872e6f5df66e4057a0341e4
Siraj Ansari <79093218+Hairy-Breeches@users.noreply.github.com>
2023-05-19 14:48:56 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb+srv://Hairy--Breeches:4XSdu4qRJSZq0NUl@cluster0.cuylh78.mongodb.net/node-angular?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Hairy--Breeches:dkDTWJrqOmn6RYGv@cluster0.cuylh78.mongodb.net/node-angular?retryWrites=true&w=majority')
 .then(() => {
   console.log('Connected to database!')
 })
